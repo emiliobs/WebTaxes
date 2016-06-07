5aeb276e51609cdd7d779ee8bf32fc271c8180b9
emilio barrera <barrera_emilio@hotmail.com>
2016-06-07 18:34:32 +0200

    Add project files.

---
+++ WebTaxes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTaxes.Startup))]
+namespace WebTaxes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
