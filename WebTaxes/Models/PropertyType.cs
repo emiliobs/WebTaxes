7357084d1576542b91e8a739d9b1ff728527f9c1
emilio barrera <barrera_emilio@hotmail.com>
2016-06-07 19:00:08 +0200

    PrpepertyType = ok

---
+++ WebTaxes/Models/PropertyType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTaxes.Models
+{
+   public class PropertyType
+    {
+        [Key]
+        public int PropertyTypeId { get; set; }
+
+        [Required(ErrorMessage = "The field description is required.")]
+        public string Description { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+    }
+}
